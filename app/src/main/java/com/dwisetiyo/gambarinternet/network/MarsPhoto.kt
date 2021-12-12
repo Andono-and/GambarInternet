d493c9503fef67c74fcc6aefe5b0d88fb42c9258
Andono-and <195410004@students.akakom.ac.id>
2021-12-12 19:30:39 +0700

    Tugas Teori

---
+++ app/src/main/java/com/dwisetiyo/gambarinternet/network/MarsPhoto.kt
@@ -0,0 +1,9 @@
+package com.dwisetiyo.gambarinternet.network
+
+import com.squareup.moshi.Json
+
+class MarsPhoto (
+    val id: String,
+    // digunakan untuk memetakan img_src dari JSON ke imgSrcUrl
+    @Json(name = "img_src") val imgSrcUrl: String
+)
